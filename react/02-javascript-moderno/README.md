c5352263a4ba7ba23ae23b87e0dc57fc5c7c62d3
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-16 20:26:39 +0200

    feat: object literal

--- react/02-javascript-moderno/README.md
+++ react/02-javascript-moderno/README.md
@@ -6,18 +6,13 @@ Temas:
 - Generar la base sobre JavaScript
 - Constantes y variables Let
 - Template String
-
-Objetos literales
-
-Arreglos
-
-Desestructruación * (sumamente importante)
-
-Promesas
-
-Fetch API
-
-Ternarios
-
-Async - Await
+- Objetos literales
+- Arreglos
+- Desestructruación * (sumamente importante)
+- Promesas
+- Fetch API
+- Ternarios
+- Async - Await
 ```
+
+---
--- react/02-javascript-moderno/src/App.jsx
+++ react/02-javascript-moderno/src/App.jsx
@@ -1,14 +1,42 @@
 import './App.css'
 
 function App() {
+  // constantes
   const firstName = 'María Eugenia';
   const lastName = 'Costa';
+  // Template string
   const fullName = `${firstName} ${lastName}`;
+  // Object literal
+  const person = {
+    firstName: 'Tony',
+    lastName: 'Stark',
+    age: 45,
+    address: {
+      city: 'New York',
+      zipCode: 123456,
+      lat: 14.234,
+      lng: 34.43434
+    }
+  }
+  const person2 = {...person}
 
   return (
-    <h1>
-      Hi, I am {fullName}, practising React.
-    </h1>
+    <div>
+      <section>
+        <h2>Template String</h2>
+        <p>Hi, I am {fullName}, practising React.</p>
+      </section>
+      <hr />
+      <section>
+        <h2>Object literal</h2>
+        <p>person.firstName: {person.firstName}</p>
+        <p>person.lastName: {person.lastName}</p>
+        <p>person.age: {person.age}</p>
+        <p>person.address.city: {person.address.city}</p>
+        <br />
+        <p>person2.firstName: {person2.firstName}</p>
+      </section>
+    </div>
   )
 }
 
