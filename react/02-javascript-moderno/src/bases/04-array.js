8f254d538b1bcd345c2356f76222b968c8c2e26b
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-16 20:42:44 +0200

    feat: arrays

--- react/02-javascript-moderno/src/bases/04-array.js
+++ react/02-javascript-moderno/src/bases/04-array.js
@@ -15,3 +15,11 @@ console.log(arreglo2); // [1, 2, 3]
 // En vez de el push usamos el spread
 let arreglo3 = [...arreglo2, 4];
 console.log(arreglo3);
+
+// método .map() de los arrays
+// Va a iterar por cada elemento del array
+// Crea un nuevo array, rompe la referencia
+
+// Como ejemplo, me creo un nuevo array que va a tener el doble del valor de los elementos, tiene un return implicito
+const arreglo4 = arreglo3.map(element => element*2);
+console.log(arreglo4);
