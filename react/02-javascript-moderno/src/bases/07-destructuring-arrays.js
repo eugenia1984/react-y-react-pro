0e0d091c095533de8512b1bfad45b0bacd66e226
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-17 19:42:26 +0200

    feat: destructuring arrays

--- react/02-javascript-moderno/src/bases/07-destructuring-arrays.js
+++ react/02-javascript-moderno/src/bases/07-destructuring-arrays.js
@@ -1,8 +1,28 @@
 // Asignación destructurante en arrays
-const personajes = ['Goku', 'Vegeta', 'Trunks'];
+const personajes = ["Goku", "Vegeta", "Trunks"];
 
 console.log(personajes);
 console.log(personajes[0]); // para el primero
 
-const [ , , p3 ] = personajes; // para el tercer persona
+const [, , p3] = personajes; // para el tercer persona
 console.log(p3);
+
+const returnArray = () => {
+  return "ABC", 123;
+};
+
+const [letras, numeros] = returnArray();
+console.log(letras, numeros);
+
+// Similar al useState de React
+const usesStates = (valor) => {
+  return [
+    valor,
+    () => {
+      console.log("Hola mundo");
+    },
+  ];
+};
+
+const [name, setName] = usesStates("Goku");
+console.log("name: ", name, "setName: ", setName);
