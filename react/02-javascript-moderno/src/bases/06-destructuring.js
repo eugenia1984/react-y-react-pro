0eba59b4e9eda5b13e8e8719c9924f02bb9912b4
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-17 18:26:56 +0200

    feat: add asignacion desestructurante

--- react/02-javascript-moderno/src/bases/06-destructuring.js
+++ react/02-javascript-moderno/src/bases/06-destructuring.js
@@ -2,7 +2,11 @@
 const person = {
   nombre: 'Tony',
   edad: 65,
-  clave: 'Ironman'
+  clave: 'Ironman',
+  latlng: {
+    lat: 12.24424,
+    lng: -12.123
+  }
 }
 
 console.log(person.nombre);
@@ -11,8 +15,9 @@ console.log(person.clave);
 
 // Asignación desestructurante
 // Podria nombrarlos en otro orden, pero queda mas claro que corresponda el orden a como viene
-const { nombre, edad, clave } = person;
-console.log('nombre: ', nombre, 'edad: ', edad, 'clave: ', clave)
+const { nombre, edad, clave, latlng: {lat, lng} } = person;
+// en latlng hago una sub asignacion desestructurante
+console.log('nombre: ', nombre, 'edad: ', edad, 'clave: ', clave, 'lat: ', lat, 'lng: ', lng)
 
 // Al desestructurar, puedo usar algunos y no todos los valores, solo los que necesito
 
