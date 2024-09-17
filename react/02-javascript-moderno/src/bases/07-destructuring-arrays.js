08f17d847a10d4f8b1f8c2a50dc2b7ed7be952bd
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-17 18:32:24 +0200

    feat: destructuring arrays

---
+++ react/02-javascript-moderno/src/bases/07-destructuring-arrays.js
@@ -0,0 +1,8 @@
+// Asignación destructurante en arrays
+const personajes = ['Goku', 'Vegeta', 'Trunks'];
+
+console.log(personajes);
+console.log(personajes[0]); // para el primero
+
+const [ , , p3 ] = personajes; // para el tercer persona
+console.log(p3);
