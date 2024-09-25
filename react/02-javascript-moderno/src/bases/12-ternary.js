a3d149f1ee3bdaf219a8d8905034bd964faee17e
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-25 18:56:27 +0200

    feat: ternario

--- react/02-javascript-moderno/src/bases/12-ternary.js
+++ react/02-javascript-moderno/src/bases/12-ternary.js
@@ -13,4 +13,10 @@ if(isActive) {
 
 // Forma corta con ternario
 const message = isActive ? 'Active' : 'Inactive';
-console.log(message)
+console.log(message);
+
+// Forma mas corta, si no quiero tener el else, quiero solo la condicion
+// Es la forma corta dle if
+// se usa en React para renderizar algo si se cumple la condicion y sino no se ejecuta nada
+const message2 = isActive && 'Active';
+console.log(message2)
