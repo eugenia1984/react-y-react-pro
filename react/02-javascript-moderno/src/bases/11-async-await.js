a720a850149fae9a10f6db4193f99980b08495d8
eugenia1984 <costamariaeugenia1@gmail.com>
2024-09-25 18:47:51 +0200

    feat: async await

--- react/02-javascript-moderno/src/bases/11-async-await.js
+++ react/02-javascript-moderno/src/bases/11-async-await.js
@@ -4,10 +4,9 @@ const getImage = async () => {
 
   // await -> espera que esta promesa termine antes de ejecutar la proxima linea de codigo
   const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
-  const data = await resp.json();
+  const { data } = await resp.json();
 
-  return data.data.images.original.url;
-
+  return data.images.original.url;
 }
 
 getImage();
