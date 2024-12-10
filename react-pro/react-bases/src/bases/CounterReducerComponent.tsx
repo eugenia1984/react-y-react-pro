b77d33f09409d79be96defe9424e6189801c42b6
eugenia1984 <costamariaeugenia1@gmail.com>
2024-12-10 13:57:44 +0100

    feat: useReducer

--- react-pro/react-bases/src/bases/CounterReducerComponent.tsx
+++ react-pro/react-bases/src/bases/CounterReducerComponent.tsx
@@ -18,12 +18,21 @@ type CounterAction =
 
 const counterReducer = ( state: CounterState, action: CounterAction ): CounterState => {
 
+  const { counter, changes} = state;
+
   switch (action.type) {
     case 'reset':
       return {
         counter: 0,
         changes: 0,
         previous: 0
+      }
+
+    case 'increaseBy':
+      return {
+        counter: counter + action.payload.value,
+        changes: changes +1,
+        previous: counter
       }
   
     default:
@@ -33,17 +42,34 @@ const counterReducer = ( state: CounterState, action: CounterAction ): CounterSt
 
 export const CounterReducerComponent = () => {
   
-  const [ { counter }, dispatch] = useReducer(counterReducer, INITIAL_STATE);
+  const [ counterState, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
-  const handleClick = () => { 
+  const handleReset = () => { 
     dispatch( { type: 'reset'})
+  }
+
+  const increaseBy = ( value: number ) => {
+    // dispatch( { type: 'increaseBy', payload: { value: value }})
+    dispatch( { type: 'increaseBy', payload: { value }})
   }
 
   return (
     <section>
-      <h2>Counter Reducer Component: {counter}</h2>
-      <button onClick={handleClick} className="counter-btn">
+      <h2>Counter Reducer Component:</h2>
+      <pre>
+        { JSON.stringify( counterState, null, 2) }
+      </pre>
+      <button onClick={() => increaseBy(1)} className="counter-btn">
         + 1
+      </button>
+      <button onClick={() => increaseBy(5)} className="counter-btn">
+        + 5
+      </button>
+      <button onClick={() => increaseBy(10)} className="counter-btn">
+        + 10
+      </button>
+      <button onClick={handleReset} className="counter-btn">
+        Reset
       </button>
     </section>
   )
