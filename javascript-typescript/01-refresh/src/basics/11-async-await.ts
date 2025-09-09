d0b3d9ebe0bd9111a921ecf8fcdf5f4586f34078
eugenia1984 <costamariaeugenia1@gmail.com>
2025-09-09 16:35:26 +0200

    feat: add async await example

---
+++ javascript-typescript/01-refresh/src/basics/11-async-await.ts
@@ -0,0 +1,25 @@
+import type { GiphyRandomResponse } from "./data/giphy-response";
+
+const API_KEY = "cHjKWJE8DGKCmixK8wqjo1Gsa7Sax0MR";
+
+const createImageInsideDOM = (url: string) => {
+  const imgElement = document.createElement("img");
+  imgElement.src = url;
+
+  document.body.append(imgElement);
+};
+
+const getRandomGifUrl = async (): Promise<string> => {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`
+  );
+
+  //  const data = (await response.json()) as GiphyRandomResponse;
+  const { data }: GiphyRandomResponse = await response.json();
+
+  return data.images.original.url;
+};
+
+getRandomGifUrl().then((url) => createImageInsideDOM(url));
+// Otro modo, ya que mando el argumento como funcion
+// getRandomGifUrl().then(createImageInsideDOM();
