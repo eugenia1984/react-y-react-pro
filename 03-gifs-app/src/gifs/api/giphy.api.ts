aaeee4d6a2bc393aa72a5d1d8243b83f3ce2383c
eugenia1984 <costamariaeugenia1@gmail.com>
2025-09-25 15:01:15 +0200

    feat: create frst custom hook

--- 03-gifs-app/src/gifs/api/giphy.api.ts
+++ 03-gifs-app/src/gifs/api/giphy.api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const giphyApi = axios.create({
   baseURL: "https://api.giphy.com/v1/gifs",
   params: {
-    lang: "en",
     api_key: import.meta.env.VITE_GIPHY_API_KEY,
+    lang: "en",
   },
 });
