c9ab73dfe158a48e83617afce7ffda9248ebe65d
eugenia1984 <costamariaeugenia1@gmail.com>
2025-10-08 15:02:11 +0200

    feat: add test configuration

---
+++ 03-gifs-app/src/GifsApp.test.tsx
@@ -0,0 +1,12 @@
+import { describe, test } from 'vitest'
+// import { describe, test, expect } from 'vitest'
+// import { render } from '@testing-library/react'
+// import { GifsApp } from './GifsApp'
+
+describe('GifsApp', () => { 
+  test('should render component properly', () => {
+    // const { container } = render(<GifsApp />);
+
+    // expect(container).toMatchSnapshot();
+  })
+})
