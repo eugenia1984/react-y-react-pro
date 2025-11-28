1ed191f3efe058d91009ba9e322bbc3f9e1f5b34
eugenia1984 <costamariaeugenia1@gmail.com>
2025-11-28 11:52:37 +0100

    feat: start with test for efects and debounce

---
+++ 03-gifs-app/src/shared/components/SearchBar.test.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, test, expect, vi } from "vitest";
+import { SearchBar } from "./SearchBar";
+
+describe('SearchBar', () => {
+  test('should render searchbar correctly', () => {
+    render(<SearchBar onQuery={() => { }} />);
+
+    expect(screen.getByRole('textbox')).toBeDefined();
+    expect(screen.getByRole('button')).toBeDefined();
+  });
+
+  test('should call onQuery with the correct value after 700ms', async () => {
+    const onQuery = vi.fn();
+
+    render(<SearchBar onQuery={onQuery} />);
+
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    // await new Promise(resolve => setTimeout(resolve, 701));
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalled();
+      expect(onQuery).toHaveBeenCalledWith('test');
+    });
+  });
+});
