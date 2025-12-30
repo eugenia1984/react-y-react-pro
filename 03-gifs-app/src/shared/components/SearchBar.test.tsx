2b0e623104c1355d7550e779ebf55042a4f56387
eugenia1984 <costamariaeugenia1@gmail.com>
2025-12-30 09:31:17 +0100

    feat: add testing

--- 03-gifs-app/src/shared/components/SearchBar.test.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.test.tsx
@@ -12,7 +12,6 @@ describe('SearchBar', () => {
 
   test('should call onQuery with the correct value after 700ms', async () => {
     const onQuery = vi.fn();
-
     render(<SearchBar onQuery={onQuery} />);
 
     const input = screen.getByRole('textbox');
@@ -23,5 +22,43 @@ describe('SearchBar', () => {
       expect(onQuery).toHaveBeenCalled();
       expect(onQuery).toHaveBeenCalledWith('test');
     });
+  });
+
+  test('should call only once with the last value (debounce)', async () => {
+    const onQuery = vi.fn();
+    render(<SearchBar onQuery={onQuery} />);
+
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, { target: { value: 't' } });
+    fireEvent.change(input, { target: { value: 'te' } });
+    fireEvent.change(input, { target: { value: 'tes' } });
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledWith('test');
+      expect(onQuery).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test('should call onQuery when button clicked with the input value', () => {
+    const onQuery = vi.fn();
+    render(<SearchBar onQuery={onQuery} />);
+
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(onQuery).toHaveBeenCalledTimes(1);
+    expect(onQuery).toHaveBeenCalledWith('test')
+  });
+
+  test('should the input has the correct placeholder value', () => {
+    const value = "Search gif";
+    render(<SearchBar onQuery={() => { }} placeholder={value} />);
+
+    // screen.debug();
+    expect(screen.getAllByPlaceholderText(value)).toBeDefined();
   });
 });
