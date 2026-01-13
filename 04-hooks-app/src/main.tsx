adb802958487a3ea7a3a898c287f8c6dcf9aabd5
eugenia1984 <costamariaeugenia1@gmail.com>
2026-01-13 15:49:33 +0100

    feat: create useTrafficLight hook

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp';
 // import { TrafficLight } from './01-useState/TrafficLight';
-import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 
 import './index.css';
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
-    <TrafficLightWithEffect />
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithHook />
   </React.StrictMode>,
 )
